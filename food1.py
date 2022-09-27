7b3699da72f7be2843e6989bfb862704940974d4
Akash-infant <99480207+Akash-infant@users.noreply.github.com>
2022-09-27 09:57:42 +0530

    Add files via upload

---
+++ food1.py
@@ -0,0 +1,32 @@
+
+#for displaying in the front page
+
+def header():
+    print("****************************")
+    print("TOMATO FOOD DELIVERY")
+    print("****************************")
+    print("FOOD THAT YOU LOVE")
+    print("from")
+    print("ZETA")
+
+
+#for entering personal details    
+def personal_details():
+    Name =  input("YOUR NAME PLEASE !")
+    Age =  int(input("ENTER YOUR AGE :"))
+    Address=input("ENTER YOUR ADDRESS :")
+    Pin = int(input("ENTER YOUR PINCODE :"))
+    Dob =  input("ENTER YOUR BIRTH DATE :")
+    Mail = input("ENTER YOUR MAIL ID :")
+
+
+#keeping a password
+def password():
+    print("KEEP A STRONG PASSWORD THAT HAS MINIMUM 8 CHARACTERS AND A NUMBER ")
+    pass1=input("ENTER YOUR PASSWORD:")
+    pass2=input("ENTER YOUR PASSWORD ONCE AGAIN :")
+    if(pass1==pass2):
+        pass
+    else:
+        print("your password is not the same ")
+    
