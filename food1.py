f2096bb932482aa4000b3bbec26370668af4a780
Akash-infant <99480207+Akash-infant@users.noreply.github.com>
2022-10-08 09:37:19 +0530

    Update food1.py

--- food1.py
+++ food1.py
@@ -1,43 +1,60 @@
-
-#for displaying in the front page
-
-def header():
-    print("****************************")
-    print("TOMATO FOOD DELIVERY")
-    print("****************************")
-    print("FOOD THAT YOU LOVE")
-    print("from")
-    print("ZETA")
+class front_page:
+    def header(self):
+        print("****************************")
+        print("TOMATO FOOD DELIVERY")
+        print("****************************")
+        print("FOOD THAT YOU LOVE")
+        print("from")
+        print("ZETA")
 
 
 #for entering personal details    
-def personal_details():
-    Name =  input("YOUR NAME PLEASE !")
-    Age =  int(input("ENTER YOUR AGE :"))
-    Address=input("ENTER YOUR ADDRESS :")
-    Pin = int(input("ENTER YOUR PINCODE :"))
-    Dob =  input("ENTER YOUR BIRTH DATE :")
-    Mail = input("ENTER YOUR MAIL ID :")
+    def personal_details(self):
+        self.Name =  input("YOUR NAME PLEASE !")
+        self.Age =  int(input("ENTER YOUR AGE :"))
+        self.Address=input("ENTER YOUR ADDRESS :")
+        self.Pin = int(input("ENTER YOUR PINCODE :"))
+        self.Dob =  input("ENTER YOUR BIRTH DATE :")
+        self.Mail = input("ENTER YOUR MAIL ID :")
     
 #display  details
-def display():
+    def display(self):
+        print(self.name)
+        print(self.Age)
+        print(self.Address)
+        print(self.Pin)
+        print(self.Dob)
+        print(self.Mail)
+    
 
 #keeping a password
-def password():
-    print("KEEP A STRONG PASSWORD THAT HAS MINIMUM 8 CHARACTERS AND A NUMBER ")
-    pass1=input("ENTER YOUR PASSWORD:")
-    pass2=input("ENTER YOUR PASSWORD ONCE AGAIN :")
-    if(pass1==pass2):
+    def password(self):
+        print("KEEP A STRONG PASSWORD THAT HAS MINIMUM 8 CHARACTERS AND A NUMBER ")
+        self.pass1=input("ENTER YOUR PASSWORD:")
+        self.pass2=input("ENTER YOUR PASSWORD ONCE AGAIN :")
+        if(self.pass1==self.pass2):
         pass
-    else:
-        print("your password is not the same ")
+        else:
+            print("your password is not the same ")
         
 
 #to show balance amount in tomato wallet
-def wallet():
-    amount = 0
-    print("your current balance for ordering is ", amount)
-    print("")
+    def wallet(self):
+        self.amount = 0
+        print("your current balance for ordering is ", amount)
+        print("ADD BALANCE TO ENJOY YOUR TASTY FOOD ")
+        self.amount=input("do you want to add money to wallet (Y/N):")
+        if(self.amount == "Y"):
+            self.amount_1=int(input("HOW MUCH WOULD YOU LIKE TO ADD :"))
+            self.amoumt+=self.amount_1
+        elif(self.amount =="N"):
+            print("YOUR WALLET BALANCE IS ",self.amount)
+
     
-   
-    
+a = front_page()
+a.header()
+a.personal_details()
+a.display()
+a.password()
+a.wallet()   
+  
